f85c6d1243204c619ce53529c20ace1165659048
daoudsamir31-svg <daoudsamir31@gmail.com>
2026-07-09 07:01:39 +0100

    add login register protection

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,58 @@ import {
 
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useAuth } from "./context/AuthContext";
+
+function ProtectedRoute({ children }) {
+
+  const { token } = useAuth();
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
 
 function App() {
+
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
+
+          <Route index element={<Home />} />
+
         </Route>
+
 
         <Route
           path="*"
           element={<Navigate to="/" replace />}
         />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
+
 
 export default App;
